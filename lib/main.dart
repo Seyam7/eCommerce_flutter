117db54016d4a6355cc1b68a66e788cf9f70b6a2
Sadman Seyam <sadman.seyam07@gmail.com>
2025-01-07 22:59:36 +0600

    firebase added to project and all setup for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:e_commerce_flutter/views/auth/login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// Import the generated file
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
