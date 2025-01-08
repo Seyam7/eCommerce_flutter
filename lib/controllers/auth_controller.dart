b3ac8398c94ee306af02c8767afd5475dfd47ac1
Sadman Seyam <sadman.seyam07@gmail.com>
2025-01-08 21:53:50 +0600

    user data stored in fire store, user logged into home page successfully

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,10 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce_flutter/views/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController extends GetxController{
   final _auth = FirebaseAuth.instance;
+  final _fireStore = FirebaseFirestore.instance;
+
   final loginKey = GlobalKey<FormState>();
+
   RxBool isLoading = false.obs;
 
   final emailController = TextEditingController();
@@ -23,6 +28,7 @@ class AuthController extends GetxController{
             email: emailController.text,
             password: passwordController.text,
         );
+        Get.offAll(()=>HomeScreen());
 
       }on FirebaseAuthException catch(error){
         Get.snackbar('Error', error.message!);
@@ -41,11 +47,22 @@ class AuthController extends GetxController{
       isLoading.value=true;
 
       try{
-        final userData = await _auth.createUserWithEmailAndPassword(
+        await _auth.createUserWithEmailAndPassword(
           email: emailController.text,
           password: passwordController.text,
-        );
-        Get.snackbar('Seccess', 'Your account has been created');
+        ).then((userC)async{
+          //create data
+          //doc(user.user!.email); this the alternate way, 1set user is model 2nd user id the current active user
+          await _fireStore.collection('users').doc(emailController.text).set({
+            'name' : nameController.text,
+            'phone' : phoneController.text,
+            'email' : emailController.text,
+            'password' : passwordController.text
+          });
+          Get.snackbar('Success', 'Your account has been created');
+
+          Get.offAll(()=>HomeScreen());
+        });
 
       }on FirebaseAuthException catch(error){
         Get.snackbar('Error', error.message!);
